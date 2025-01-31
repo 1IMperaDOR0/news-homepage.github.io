eed09702b44e95fb672a686f96f549803c75747f
1IMperaDOR0 <lucaslucore@gmail.com>
2025-01-31 07:51:31 -0300

    update files

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,17 @@
 let menu = document.querySelector(".menu").addEventListener("click", () => {
     let nav = document.querySelector('nav')
     nav.style.display = 'block'
+    let block = document.querySelector(".block")
+    block.style.display = 'block'
+    let menu = document.querySelector(".menu")
     menu.style.display = 'none'
 })
 
 let close = document.querySelector(".close").addEventListener("click", () => {
     let nav = document.querySelector('nav')
     nav.style.display = 'none'
+    let block = document.querySelector(".block")
+    block.style.display = 'none'
+    let menu = document.querySelector(".menu")
     menu.style.display = 'block'
 })
